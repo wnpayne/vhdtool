524024904381cf6e150d313794ffcb9c61b96591
Wes Payne <payne.wes@gmail.com>
2014-01-19 22:37:23 -0800

    fixed bits->bytes and added comments. Also added a checksumming function (empty for now).

--- vhdtool.c
+++ vhdtool.c
@@ -37,7 +37,8 @@ void fixString(int originalLen, char *originalString, char *fixedString) {
 
 }
 
-void printbits(char *toprint,int len) {
+// provide a pointer to a "string" (actually just a memory address), and the number of bytes to print.
+void printbytes(char *toprint,int len) {
 
     int i=0;
     for (i=0;i<len;i++) {
@@ -54,34 +55,40 @@ void footer_print(VHDFOOTER in_footer) {
 	fixString(4,in_footer.cApp,fixed_cApp);
 	printf("cookie:\t\"%s\"\ncapp:\t\"%s\"",fixed_cookie,fixed_cApp);
 	printf("\nFeatures\t");
-	printbits((char *) &in_footer.features,4);
+	printbytes((char *) &in_footer.features,4);
 	printf("\nFile Format\t");
-	printbits((char *) &in_footer.fileformat,4);
+	printbytes((char *) &in_footer.fileformat,4);
 	printf("\nCreatorr Ver:\t");
-	printbits((char *) &in_footer.cVer,4);
+	printbytes((char *) &in_footer.cVer,4);
 	printf("\nCreator HostOS:\t");
-	printbits((char *) &in_footer.cOS,4);
+	printbytes((char *) &in_footer.cOS,4);
 	printf("\nData Offset:\t");
-	printbits((char *) &in_footer.dataoffset,8);
+	printbytes((char *) &in_footer.dataoffset,8);
 	printf("\nTime Stamp\t");
-	printbits((char *) &in_footer.timestamp,4);
+	printbytes((char *) &in_footer.timestamp,4);
 	printf("\nOriginal Size\t");
-	printbits((char *) &in_footer.originalsize,8);
+	printbytes((char *) &in_footer.originalsize,8);
 	printf("\nCurrent Size\t");
-	printbits((char *) &in_footer.currentsize,8);
+	printbytes((char *) &in_footer.currentsize,8);
 	printf("\nDisk Geo:\t");
-	printbits((char *) &in_footer.diskgeo,4);
+	printbytes((char *) &in_footer.diskgeo,4);
 	printf("\nDisk Type\t");
-	printbits((char *) &in_footer.disktype,4);
+	printbytes((char *) &in_footer.disktype,4);
 	printf("\nChecksum\t");
-	printbits((char *) &in_footer.checksum,4);
+	printbytes((char *) &in_footer.checksum,4);
 	printf("\nSaved State:\t");
-	printbits((char *) &in_footer.savedstate,1);
+	printbytes((char *) &in_footer.savedstate,1);
 	printf("\nuuid:\t");
-	printbits((char *) &in_footer.uuid,16);
+	printbytes((char *) &in_footer.uuid,16);
 	printf("\n\n");
     
 	printf("structSize:\t%lu\n",sizeof(VHDFOOTER));
+}
+
+unsigned int footer_checksum(VHDFOOTER in_footer) {
+	unsigned int checksum = 0;
+
+	return checksum;	
 }
 
 int main(int argc, char *argv[]) {
@@ -95,6 +102,10 @@ int main(int argc, char *argv[]) {
     fclose(myfile);
 
     footer_print(footer);
+
+    printf("checking checksum...:\n");
+    printf("%d\n",footer.checksum);
+
 
     return 0;
 }
